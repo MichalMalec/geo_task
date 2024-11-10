3da76b87193f98b5ff2ed326d0306bfc448b0443
michal <michalmalec5@o2.pl>
2024-11-10 18:26:37 +0100

    add new ipstack services

---
+++ app/services/ipstack_service.rb
@@ -0,0 +1,12 @@
+# app/services/ipstack_service.rb
+require 'httparty'
+
+class IpstackService
+  BASE_URL = "http://api.ipstack.com"
+  ACCESS_KEY = ENV['IPSTACK_API_KEY']
+
+  def self.get_geolocation(ip_or_url)
+    response = HTTParty.get("#{BASE_URL}/#{ip_or_url}?access_key=#{ACCESS_KEY}")
+    response.parsed_response if response.code == 200
+  end
+end
